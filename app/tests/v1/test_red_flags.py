97ffed2f68fe76345c00a02c77f309be93c9780d
Edward Njoroge <edwardnjorogekahara@gmail.com>
2018-11-30 10:44:54 +0300

    [finishes #162216975] create a get a specific red flag endpoint

--- app/tests/v1/test_red_flags.py
+++ app/tests/v1/test_red_flags.py
@@ -22,3 +22,17 @@ class TestRedFlags(TestCase):
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data, {"status": 200, "data": self.red_flags.db})
+
+    def test_get_specific_red_flag(self):
+        """This test ensures that the api endpoint gets a specific red-flag record"""
+        response = self.app.get('/api/v1/red-flag/1')
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, {"status": 200, "data": [self.red_flags.db[0]]})
+
+    def test_red_flag_not_found(self):
+        """This test ensures that when a specific red-flag record does not exist, we get an error message saying record not found."""
+        response1 = self.app.get('/api/v1/red-flag/0')
+        data1 = json.loads(response1.data)
+        self.assertEqual(response1.status_code, 404)
+        self.assertEqual(data1, {"status": 404, "error": "Red-flag record not found"})
