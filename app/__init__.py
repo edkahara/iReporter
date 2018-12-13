94bc3a60fc7c8f9e23171ba4edb35e97a2a897b4
Edward Njoroge <edwardnjorogekahara@gmail.com>
2018-12-14 00:29:22 +0300

    [finishes #162650787] create v2 logout endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
-import os
 from flask import Flask, Blueprint
 from flask_restful import Resource, Api
 from flask_jwt_extended import JWTManager
 
 from .api.v2 import version_two as v2
+from .api.v2.users import blacklist
 from instance.database import DBModel
 from instance.config import config
 
@@ -11,7 +11,12 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     app.register_blueprint(v2)
-    jwt = JWTManager(app)
     with app.app_context():
         DBModel().create_tables()
+
+    jwt = JWTManager(app)
+    @jwt.token_in_blacklist_loader
+    def check_if_token_in_blacklist(decrypted_token):
+        jti = decrypted_token['jti']
+        return jti in blacklist
     return app
---
+++ app/utils/test_variables.py
@@ -0,0 +1,75 @@
+new_user_same_passwords = {
+    "firstname": "Bo",
+    "lastname": "Rai Cho",
+    "email": "boraicho@gmail.com",
+    "phonenumber": "+2540123456789",
+    "username": "boraicho",
+    "password": "boraicho",
+    "password_confirmation": "boraicho"
+}
+new_user_different_passwords = {
+    "firstname": "Bo",
+    "lastname": "Rai Cho",
+    "email": "boraicho@gmail.com",
+    "phonenumber": "+2540123456789",
+    "username": "boraicho",
+    "password": "boraicho",
+    "password_confirmation": "bo rai cho"
+}
+new_user_taken_email = {
+    "firstname": "Bo",
+    "lastname": "Rai Cho",
+    "email": "boraicho@gmail.com",
+    "phonenumber": "+254123456789",
+    "username": "boraico",
+    "password": "boraicho",
+    "password_confirmation": "boraicho"
+}
+new_user_taken_phonenumber = {
+    "firstname": "Bo",
+    "lastname": "Rai Cho",
+    "email": "boraich@gmail.com",
+    "phonenumber": "+2540123456789",
+    "username": "boracho",
+    "password": "boraicho",
+    "password_confirmation": "boraicho"
+}
+new_user_taken_username = {
+    "firstname": "Bo",
+    "lastname": "Rai Cho",
+    "email": "boraico@gmail.com",
+    "phonenumber": "+2541234567890",
+    "username": "boraicho",
+    "password": "boraicho",
+    "password_confirmation": "boraicho"
+}
+new_user_invalid_email = {
+    "firstname": "Bo",
+    "lastname": "Rai Cho",
+    "email": "boraichogmail.com",
+    "phonenumber": "+2540123456789",
+    "username": "boraico",
+    "password": "boraicho",
+    "password_confirmation": "boraicho"
+}
+new_user_invalid_username = {
+    "firstname": "Bo",
+    "lastname": "Rai Cho",
+    "email": "boraico@gmail.com",
+    "phonenumber": "+2540123456789",
+    "username": " ",
+    "password": "boraicho",
+    "password_confirmation": "boraicho"
+}
+new_user_login_correct_details = {
+    "username": "boraicho",
+    "password": "boraicho"
+}
+new_user_login_incorrect_password = {
+    "username": "boraicho",
+    "password": "bo rai cho"
+}
+new_user_login_nonexistent_username = {
+    "username": "xyz",
+    "password": "boraicho"
+}
